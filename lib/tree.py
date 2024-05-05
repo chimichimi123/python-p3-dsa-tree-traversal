3eff3a555a097ac3b1dedd5f232b270f32fcee2e
Christopher Cantu <chrischimi02@gmail.com>
2024-05-05 18:55:57 -0500

    complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,15 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    return self._search_tree(self.root, id)
+
+  def _search_tree(self, node, id):
+    if node is None:
+      return None
+    if node['id'] == id:
+      return node
+    for child in node['children']:
+      result = self._search_tree(child, id)
+      if result is not None:
+        return result
+    return None
